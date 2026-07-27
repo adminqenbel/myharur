b841bae278f4cc5b8029f32682448059ec82e5e1
HEMAPRIYAN R K <hemapriyankuppusamy07@gmail.com>
2026-07-27 20:50:22 +0530

    fix: import Query from fastapi in auth.py

--- backend/app/api/endpoints/auth.py
+++ backend/app/api/endpoints/auth.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 from typing import Any, Optional
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException, Query
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
